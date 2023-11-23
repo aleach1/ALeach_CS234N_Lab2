e4598b3d04db1356bfaea190dd15a8859f43d4fe
Aiden Leach <leacha@my.lanecc.edu>
2023-11-22 21:49:40 -0800

    finished product

--- MMABooksADO2022/MMABooksBusinessClasses/Product.cs
+++ MMABooksADO2022/MMABooksBusinessClasses/Product.cs
@@ -6,5 +6,28 @@ namespace MMABooksBusinessClasses
 {
     public class Product
     {
+        public Product() { }
+
+        public Product(string description, int onHandQuantity, string productCode, decimal unitPrice)
+        {
+            Description = description;
+            OnHandQuantity = onHandQuantity;
+            ProductCode = productCode;
+            UnitPrice = unitPrice;
+        }
+
+        public string Description { get; set; }
+
+        public int OnHandQuantity { get; set; }
+        
+        public string ProductCode { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
+    
 }
